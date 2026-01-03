ccb250426a5622ecc1f7a10765d44804491b6336
amanamitabh <amanamitabh@gmail.com>
2026-01-03 20:22:22 +0400

    Added threaded video capture

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import cv2
 import os
-import whisper
-import json
+import time
+import threading
+import queue
 from dotenv import load_dotenv
+
+def now():
+    # Get current time in global clock
+    return time.perf_counter()
+
 
 # Adds variables from .env file to program environment variables
 load_dotenv()
@@ -11,27 +17,39 @@ load_dotenv()
 ffmpeg_bin_path = os.environ["FFMPEG_BIN_PATH"]
 os.environ["PATH"] = f"{ffmpeg_bin_path}{os.pathsep}{os.environ.get('PATH','')}"
 
-# Get video directory path and join path to video
-video_dir = os.environ["VIDEO_DIR"]
-video = os.path.join(video_dir, "vid2.mp4")
+# Queues for processing audio and video
+video_queue = queue.Queue(maxsize=30)
+audio_queue = queue.Queue(maxsize=30)
 
-# Capture Video
-cap = cv2.VideoCapture(video)
+# Thread safe signal flag
+STOP = threading.Event()
 
-# Get FPS from properties and calculate delay
-fps = cap.get(cv2.CAP_PROP_FPS)
-if fps > 0:
-    delay = int(1000/fps)   # Calculate delay in ms
-else:
-    delay = 33    # Set default delay in case fps is 0s
+# Video Capture Thread
+def video_capture():
+    cap = cv2.VideoCapture(0)
 
-# Load the whisper model (use base or small)
-model = whisper.load_model("base")
+    while not STOP.is_set():
+        ret, frame = cap.read()
 
-# Transcribe and print the result
-result = model.transcribe(video, fp16=False)
-print(result)
+        if not ret:
+            continue
 
-# Free resources and destroy windows
-cap.release()
-cv2.destroyAllWindows()
+        # Get current timestamp
+        timestamp = now()
+
+        # Enqueue the frame if video queue is empty, or else frame is dropped
+        try:
+            video_queue.put_nowait((timestamp, frame))
+            print(f"[VIDEO]: {timestamp:.6f}")
+        
+        except queue.Full:
+            pass
+
+        # Display the frame
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Free allocated resources
+    cap.release()
+    cv2.destroyAllWindows()
